12b18f6094a13281a36044e78cf615fae9f38a0d
Jessica Murphy <jessicaammurphy@gmail.com>
2025-02-12 20:52:51 -0500

    temp build system

--- Gamedev/Assets/scripts/PlacementSystem.cs
+++ Gamedev/Assets/scripts/PlacementSystem.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
@@ -10,18 +12,68 @@ public class PlacementSystem : MonoBehaviour
     private InputManager inputManager;
     [SerializeField]
     private Grid grid;
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+    private int selectedObjectIndex = -1;
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+
+        selectedObjectIndex = database.objectsData.FindIndex(database => database.ID == ID);
+        if(selectedObjectIndex < 0)
+        {
+            Debug.LogError($"no ID found {ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if(inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+
+        //mousePosition.y = 0f;
+
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        //Vector3 worldPosition = grid.CellToWorld(gridPosition);
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        //mouseIndicator.transform.position = mousePosition;
+        //cellIndicator.transform.position = worldPosition;
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+    }
 
     private void Update()
     {
+      if(selectedObjectIndex < 0)
+      return;
+       
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-
-        mousePosition.y = 0f;
-
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
-        Vector3 worldPosition = grid.CellToWorld(gridPosition);
-
         mouseIndicator.transform.position = mousePosition;
-        cellIndicator.transform.position = worldPosition;
-
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
