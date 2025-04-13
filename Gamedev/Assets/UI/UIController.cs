aed584b7d2ed02d159ebe56e48691c711cbade63
Nicole Walsh <nlwlynn@gmail.com>
2025-04-13 14:56:35 -0400

    Update

--- Gamedev/Assets/UI/UIController.cs
+++ Gamedev/Assets/UI/UIController.cs
@@ -607,8 +607,12 @@ public class UIController : MonoBehaviour
         shopPanel.SetActive(true);
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         // Force the shop buttons to reflect current coin amount
         FindObjectOfType<ShopManager>()?.CheckPurchaseable();
+=======
+        soundEffects.PlayClickSound(); // Click SFX
+>>>>>>> Stashed changes
 =======
         soundEffects.PlayClickSound(); // Click SFX
 >>>>>>> Stashed changes
